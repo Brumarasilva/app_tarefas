053fa373954946d8f58688c3304f342f2069ec93
Bruna Mara Silva <cristalbrumara@gmail.com>
2025-08-07 11:07:59 -0300

    adicionando migration, controller configurando appdbcontext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using app_tarefas.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace app_tarefas.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+
+    {
+
+    }
+    public DbSet<Tipo> Tipos { get; set; } = null!;
+}
